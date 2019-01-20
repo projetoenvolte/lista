6f96235662a2147f32e878315df5d0cfe40b736d
projetoenvolte <46864607+projetoenvolte@users.noreply.github.com>
2019-01-20 10:51:07 -0200

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,2 +1,8 @@
-var nome = "Luis";
-document.write(nome);
+var form = "<form method='POST'>";
+var form_close = "</form>";
+
+var input_username = "<input type='text' name='username' placeholder='Usuário'/>";
+var input_password = "<input type='password' name='password' placeholder='Senha'/>";
+var input_submit = "<input type='submit' name='logar' value='Logar'/>";
+
+document.write(form + input_username + input_password + input_submit + form_close);
